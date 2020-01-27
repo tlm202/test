5171a0bff583bd2e7ed349828d372fdee974795e
qfn135 <qfn135@my.utsa.edu>
2020-01-27 17:11:21 -0600

    initial commit

---
+++ Yuhh/src/yuhh/Lit.java
@@ -0,0 +1,20 @@
+package yuhh;
+
+import java.util.Scanner;
+
+public class Lit {
+
+	public static void main(String[] args) {
+		
+	 System.out.println("Enter your name: ");
+		
+	 Scanner in  = new Scanner(System.in);
+	 String s = in.nextLine();
+	 
+	  
+
+	 in.close();
+	}
+	
+}	
+
